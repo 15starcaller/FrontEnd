729c180d76990b562b4065eace61673de7f8dd32
Бужаев Владислав <csgoop@mail.ru>
2019-11-10 12:15:12 +0300

    Restart btn

--- l5/script.js
+++ l5/script.js
@@ -22,27 +22,94 @@ function add() {
 //динамические вещи это джава
 
 var current = "green";
-let colors =
-    ["none","none","none","none","none","none","none","none","none"];
+let colors = ["none", "none", "none", "none", "none", "none", "none", "none", "none"];
 
+function restart {
+    
+}
+    
 function turn(number) {
     //alert(number);
-    console.log(number);
-    $("#s" + number).css("background-color", current);
-    colors[number-1]=current;
+    //console.log(number);
+
+    //$("#s" + number).css("background-color", current); //без анимаций
+    $("#s" + number).animate({
+        borderColor: "white",
+        marginTop: 15 ,
+        backgroundColor : current
+    }, 500)
+    colors[number - 1] = current;
     console.log(colors);
-    
+
     if (current === "green") {
 
         current = "red";
     } else {
         current = "green";
     }
-    $("#s"+number).prop("disabled", true);
-    
-    
-    for(let i = 0; i<10; i++){ 
-       //17 условий  
+    $("#s" + number).prop("disabled", true);
+    var result = check();
+    console.log(result);
+    if(result!="none"){
+        for(let i=1; i<10; i++){
+            $("#s" + i).prop("disabled", true);
+        }
     }
+    
+    if(result!="none"){
+        $("#result").animate({
+            backgroundColor:result,
+        },200);
+        $("#result").text("Winner is")
+    }
+    
+
+    if (check() == "green") {
+
+    }
+
+
+    for (let i = 0; i < 10; i++) {
+
+    }
+
 }
 
+function check() {
+    for (let i = 0; i < 7; i += 3) {
+        if (colors[0 + i] == colors[1 + i] == colors[2 + i] && colors[2 + i] != "none") {
+            return colors[0 + i];
+        }
+    }
+
+    for (let i = 0; i < 3; i += 1) {
+        if (colors[3 + i] == colors[4 + i] == colors[5 + i] && colors[5 + i] != "none") {
+            return colors[0 + i];
+        }
+    }
+
+
+    if (colors[0] == colors[4] && colors[4] == colors[8] && colors[0] !=
+        "none") {
+        return colors[0];
+    }
+    if (colors[2] == colors[4] == colors[4] && colors[6] && colors[2] !=
+        "none") {
+        return colors[2];
+    }
+
+    let wasNone = false;
+    for (let i = 0; i < 9; i++) {
+        if (colors[i] == "none") {
+            wasNone = true;
+        }
+
+    }
+    if (wasNone == false) {
+        return "draw";
+    }
+    return "none";
+
+
+    // return "nothing";
+}
